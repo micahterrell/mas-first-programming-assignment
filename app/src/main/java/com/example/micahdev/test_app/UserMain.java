c400cce28832b4a2d46e1c5e00e1cc0b92f60495
micahterrell <micahterrell@users.noreply.github.com>
2018-09-04 13:00:21 -0400

    Added some buttons

--- app/src/main/java/com/example/micahdev/test_app/UserMain.java
+++ app/src/main/java/com/example/micahdev/test_app/UserMain.java
@@ -1,7 +1,10 @@
 package com.example.micahdev.test_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class UserMain extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class UserMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
+
+        //Setup submit button listener
+        findViewById(R.id.weatherButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Weather.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/micahdev/test_app/Weather.java
@@ -0,0 +1,43 @@
+package com.example.micahdev.test_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class Weather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="api.openweathermap.org/data/2.5/weather?q=London,appid=8c1e0c54cd5d593c2aa516dda23621c4";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("WEATHER", "Response is " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("WEATHER", "Volley error " + error.toString());
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
