6cea4330282c4383e4b95c8e1992e166a2b0e0b4
micahterrell <micahterrell@users.noreply.github.com>
2018-09-04 16:47:38 -0400

    Implemented random color button

--- app/src/main/java/com/example/micahdev/test_app/UserMain.java
+++ app/src/main/java/com/example/micahdev/test_app/UserMain.java
@@ -1,10 +1,13 @@
 package com.example.micahdev.test_app;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import java.util.concurrent.ThreadLocalRandom;
+
+import java.util.Random;
 
 public class UserMain extends AppCompatActivity {
 
@@ -19,6 +22,16 @@ public class UserMain extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), Weather.class);
                 startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.randomButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Change to a random background color
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                view.setBackgroundColor(color);
             }
         });
     }
